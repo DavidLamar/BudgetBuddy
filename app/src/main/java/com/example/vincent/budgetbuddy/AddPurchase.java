84a79d8853ee319f410c04773e4ac636c0c18231
Vincent Ball <vincentbll07@gmail.com>
2016-02-10 19:03:30 -0500

    Initial commit

---
+++ app/src/main/java/com/example/vincent/budgetbuddy/AddPurchase.java
@@ -0,0 +1,98 @@
+package com.example.vincent.budgetbuddy;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.Toast;
+
+public class AddPurchase extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
+
+    private Spinner spinner;
+    private static final String[] paths = {"Cash", "Check", "Credit", "Debit"};
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_purchase);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        spinner = (Spinner) findViewById(R.id.spinner1);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(AddPurchase.this,
+                android.R.layout.simple_spinner_item, paths);
+
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+        spinner.setOnItemSelectedListener(this);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+
+    public void displayInfo(View view) {
+
+        EditText money = (EditText) findViewById(R.id.moneyField);
+        EditText place = (EditText) findViewById(R.id.placeField);
+
+        /*
+        Toast.makeText(getApplicationContext(), "Purchase amount: $" + amount.getText().toString() + "\n" + "Place purchased: "
+                + place.getText().toString(), Toast.LENGTH_LONG).show();
+        */
+
+        Toast.makeText(getApplicationContext(), "Purchase added", Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        switch (position) {
+            case 0:
+                // Whatever you want to happen when the first item gets selected
+                break;
+            case 1:
+                // Whatever you want to happen when the second item gets selected
+                break;
+            case 2:
+                // Whatever you want to happen when the thrid item gets selected
+                break;
+        }
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+
+    }
+
+    public void selectPurchase(View v){
+        Button button = (Button)v;
+        startActivity(new Intent(getApplicationContext(), AddPurchase.class));
+    }
+
+    public void selectGoals(View v){
+        Button button = (Button)v;
+        startActivity(new Intent(getApplicationContext(), Goals.class));
+    }
+
+    public void selectSpending(View v){
+        Button button = (Button)v;
+        startActivity(new Intent(getApplicationContext(), Spending.class));
+    }
+
+    public void selectHome(View v){
+        Button button = (Button)v;
+        startActivity(new Intent(getApplicationContext(), BudgetBuddy.class));
+    }
+}
---
+++ app/src/main/java/com/example/vincent/budgetbuddy/Goals.java
@@ -0,0 +1,56 @@
+package com.example.vincent.budgetbuddy;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Button;
+
+public class Goals extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_goals);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+
+    public void selectNewGoal(View v){
+        Button button = (Button)v;
+        startActivity(new Intent(getApplicationContext(), SetGoal.class));
+    }
+
+    public void selectPurchase(View v){
+        Button button = (Button)v;
+        startActivity(new Intent(getApplicationContext(), AddPurchase.class));
+    }
+
+    public void selectGoals(View v){
+        Button button = (Button)v;
+        startActivity(new Intent(getApplicationContext(), Goals.class));
+    }
+
+    public void selectSpending(View v){
+        Button button = (Button)v;
+        startActivity(new Intent(getApplicationContext(), Spending.class));
+    }
+
+    public void selectHome(View v){
+        Button button = (Button)v;
+        startActivity(new Intent(getApplicationContext(), BudgetBuddy.class));
+    }
+
+}
