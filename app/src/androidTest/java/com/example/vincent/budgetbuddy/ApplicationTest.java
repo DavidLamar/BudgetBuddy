03addb0071b577b56ce3082da22c45eff3627026
DavidLamar <dmlamar@mtu.edu>
2016-03-18 19:36:31 -0400

    Did some testing with FileIO; updated goals saving and loading

--- app/src/androidTest/java/com/example/vincent/budgetbuddy/ApplicationTest.java
+++ app/src/androidTest/java/com/example/vincent/budgetbuddy/ApplicationTest.java
@@ -1,13 +1,17 @@
 package com.example.vincent.budgetbuddy;
 
 import android.app.Application;
+import android.support.v7.app.AppCompatActivity;
 import android.test.ApplicationTestCase;
+import android.test.InstrumentationTestCase;
 
 /**
  * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
  */
-public class ApplicationTest extends ApplicationTestCase<Application> {
-    public ApplicationTest() {
-        super(Application.class);
+public class ApplicationTest extends InstrumentationTestCase {
+    public void test() throws Exception{
+
+
     }
+
 }
