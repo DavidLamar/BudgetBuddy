5baf48533576467b0f3b4bd70541e386475e0a3a
vball15 <vincentbll07@gmail.com>
2016-04-14 23:11:37 -0400

    Added to settings page..

--- app/src/main/java/com/example/vincent/budgetbuddy/Settings.java
+++ app/src/main/java/com/example/vincent/budgetbuddy/Settings.java
@@ -7,6 +7,8 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
+import android.widget.RatingBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -17,6 +19,9 @@ import com.example.vincent.budgetbuddy.R;
 public class Settings extends AppCompatActivity {
 
     private FileIO fio;
+    private RatingBar ratingBar;
+    private TextView txtRatingValue;
+    private Button btnSubmit;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,6 +30,10 @@ public class Settings extends AppCompatActivity {
         setContentView(R.layout.activity_settings_);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        addListenerOnRatingBar();
+        addListenerOnButton();
+
     }
 
     public void deleteUserData(View view){
@@ -33,6 +42,55 @@ public class Settings extends AppCompatActivity {
         startActivity(new Intent(getApplicationContext(), BudgetBuddy.class));
     }
 
+
+    public void selectHome(View v){
+        Button button = (Button)v;
+        startActivity(new Intent(getApplicationContext(), BudgetBuddy.class));
+    }
+
+    public void selectAbout(View v){
+        Button button = (Button)v;
+        startActivity(new Intent(getApplicationContext(), About.class));
+    }
+
+
+    public void addListenerOnRatingBar() {
+
+        ratingBar = (RatingBar) findViewById(R.id.ratingBar);
+        //txtRatingValue = (TextView) findViewById(R.id.txtRatingValue);
+
+        //if rating value is changed,
+        //display the current rating value in the result (textview) automatically
+        ratingBar.setOnRatingBarChangeListener(new RatingBar.OnRatingBarChangeListener() {
+            public void onRatingChanged(RatingBar ratingBar, float rating,
+                                        boolean fromUser) {
+
+                //txtRatingValue.setText(String.valueOf(rating));
+
+            }
+        });
+    }
+
+    public void addListenerOnButton() {
+
+        ratingBar = (RatingBar) findViewById(R.id.ratingBar);
+        btnSubmit = (Button) findViewById(R.id.RateUs);
+
+        //if click on me, then display the current rating value.
+        btnSubmit.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+
+                Toast.makeText(getApplicationContext(),
+                        String.valueOf(ratingBar.getRating()),
+                        Toast.LENGTH_SHORT).show();
+
+            }
+
+        });
+
+    }
 
 
 }
